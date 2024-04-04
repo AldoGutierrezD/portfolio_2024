927a2d1021e8fcc5f501ad1eb0daf62d5288d1b7
AldoGutierrezD <aldogd24@gmail.com>
2024-04-03 20:45:37 -0600

    change outdir

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,8 +6,4 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), astroI18next()],
-  outDir: "./docs",
-  build: {
-    assets: "a",
-  },
 });
